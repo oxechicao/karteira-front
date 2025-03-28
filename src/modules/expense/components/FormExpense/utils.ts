cb20620512c90cedf3253d23e22d75a26acd0fd0
Chicão Thiago <fthiagogv@gmail.com>
2025-03-28 19:05:14 -0300

    chore: utils should be removed soon

--- src/modules/expense/components/FormExpense/utils.ts
+++ src/modules/expense/components/FormExpense/utils.ts
@@ -41,3 +41,55 @@ export const updatePurchaseAt = (
   createFormProps.form?.setFieldValue(["timeline", "purchasedAt"], newDate);
   return newDate;
 };
+
+// const [manualPurchasedDate, setManualPurchasedDate] = useState<DateTime>();
+
+// const definitionType = Form.useWatch(
+//   ["definition", "type", "name"],
+//   createFormProps.form,
+// );
+// const frequencyType = Form.useWatch(
+//   ["definition", "frequency", "name"],
+//   createFormProps.form,
+// );
+
+// createFormProps.onValuesChange = (changedValues, allValues) => {
+//   if (changedValues?.installment?.total !== undefined) {
+//     updateLastPaymentAt(
+//       createFormProps,
+//       changedValues.installment.total,
+//       allValues?.timeline?.purchasedAt,
+//     );
+//   }
+
+//   if (changedValues?.installment?.current !== undefined) {
+//     const updated = updatePurchaseAt(
+//       createFormProps,
+//       changedValues.installment.current,
+//       allValues?.timeline?.purchasedAt,
+//       manualPurchasedDate,
+//     );
+
+//     if (updated && allValues?.installment?.total !== undefined) {
+//       updateLastPaymentAt(
+//         createFormProps,
+//         allValues.installment.total,
+//         updated,
+//       );
+//     }
+//   }
+
+//   if (
+//     changedValues?.timeline?.purchasedAt ||
+//     changedValues?.timeline?.purchasedAt === null
+//   ) {
+//     setManualPurchasedDate(changedValues.timeline.purchasedAt);
+//   }
+
+//   if (changedValues?.flags?.isRecurrent) {
+//     createFormProps?.form?.setFieldValue(
+//       ["installment", "current"],
+//       changedValues.flags.isRecurrent ? 0 : undefined,
+//     );
+//   }
+// };
